50c137500bf047d9ebfc829a0364ab6fd2f4f838
Amrita <amrita@code.com>
2021-08-14 17:35:45 +0530

    upload

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports={
-    MONGOURI:process.env.MOGOURI,
-    JWT_SECRET:process.env.JWT_SEC,//should be unique 
+    MONGOURI:"mongodb+srv://amrita:hEcF6q9QUuuExkJY@cluster0.xlwbk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET:"efefefefefefe1",//should be unique 
     SENDGRID_API_KEY:'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'
     // EMAIL:"http:localhost:3000"
 }
