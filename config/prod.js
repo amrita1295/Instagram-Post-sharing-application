ca5f48c3666d17d578bc8e5746fe5b5897b1398c
Amrita <amrita@code.com>
2021-08-14 12:32:01 +0530

    BUILD

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports={
+    MONGOURI:process.env.MOGOURI,
+    JWT_SECRET:process.env.JWT_SEC,//should be unique 
+    SENDGRID_API_KEY:'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'
+    // EMAIL:"http:localhost:3000"
+}
