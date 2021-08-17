5ca133ef4d3e801a7f51d2a9bac72739d7c6f5a4
Amrita <amrita@code.com>
2021-08-17 14:12:17 +0530

    deleted

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports={
     MONGOURI:"mongodb+srv://amrita:hEcF6q9QUuuExkJY@cluster0.xlwbk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     ,JWT_SECRET:"efefefefefefe1",//should be unique 
-    SENDGRID_API_KEY:'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'
+    SENDGRID_API_KEY:'**********************************'
     // EMAIL:"http:localhost:3000"
 }
