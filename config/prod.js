b912b20d0fef5807de330da3bf8747e67a488bdf
Amrita <amrita@code.com>
2021-08-17 14:27:14 +0530

    deleted

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports={
+    MONGOURI:process.env.MOGOURI,
+    JWT_SECRET:process.env.JWT_SEC,//should be unique 
+    SENDGRID_API_KEY:'*****************'
+    // EMAIL:"http:localhost:3000"
+}
